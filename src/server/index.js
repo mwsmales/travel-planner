425baed4a548165b6834becca3d13a87e5aed50b
Michael S <michael.smales@gmail.com>
2022-04-19 11:04:14 -0500

    add basic express server setup

---
+++ src/server/index.js
@@ -0,0 +1,23 @@
+// require dotenv for environmental variables
+const dotenv = require('dotenv');
+dotenv.config();
+
+//initialize app
+const express = require('express');
+const app = express();
+
+/* dependencies */
+const bodyParser = require('body-parser');
+const cors = require ('cors');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(cors());
+
+// refer local server to ./dist/ files
+app.use(express.static('dist'));
+
+// initalize serer
+const port = 8081;
+app.listen(port, function(){
+    console.log(`App listening on port ${port}!`)
+})
