23654102de6e76acbb4c722ad94612d250deaa84
Michael S <michael.smales@gmail.com>
2022-04-18 22:11:30 -0500

    add basic environment setup

---
+++ webpack.prod.js
@@ -0,0 +1,42 @@
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+
+
+module.exports = {
+    entry: './src/client/index.js',
+    mode: 'production',
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
+            }
+        ]
+    },
+    output: {
+        libraryTarget: 'var',
+        library: 'Client',
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html",
+        }),
+        new MiniCssExtractPlugin(),
+    ],
+    optimization: {
+        minimizer: [
+          new TerserPlugin({}),
+          new CssMinimizerPlugin(),
+        ],
+    }, 
+}
