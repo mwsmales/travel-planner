d237056145d0329f4180fae694cae0fcf175456f
Michael S <michael.smales@gmail.com>
2022-04-23 17:34:52 -0500

    refactor - add object to store trip data

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -7,46 +7,61 @@ import {
     getForecastWeather
 } from './apiFunctions'
 
+let trips = {tripCount: 0, idCount: 0, tripData : []}; // global object to hold trip info 
 
 // main function 
 async function addTrip(event) {
     event.preventDefault();
 
+    // initialize new tripdata entry    
+    let tripData = {
+        'id': trips['idCount']+1,
+        'location':{}
+    }  
+    console.log(trips);
+
     // TODO: update this with some sort of location validation, otherwise print an error to the UI    
     // get values from form
-    const destCountry = document.getElementById('inputCountry').value;
-    const destCity = document.getElementById('inputCity').value;
-    const tripDate = document.getElementById('dateInput').value;
+    tripData['location']['country'] = document.getElementById('inputCountry').value;
+    tripData['location']['city'] = document.getElementById('inputCity').value;
+    tripData['date'] = document.getElementById('dateInput').value;
+    console.log(tripData);
 
     // retrieve API keys from backend
     const geoNamesKey = await getApiKey('http://localhost:8081/getGeoNamesKey');
     const weatherbitKey = await getApiKey('http://localhost:8081/getWeatherbitKey');
     const pixabayKey = await getApiKey('http://localhost:8081/getPixabayKey');
 
-    console.log(`GeoNames Key: ${geoNamesKey}`)
-    console.log(`Weatherbit Key: ${weatherbitKey}`)
-    console.log(`Pixabay Key: ${pixabayKey}`)
+    console.log(`GeoNames Key: ${geoNamesKey}`);
+    console.log(`Weatherbit Key: ${weatherbitKey}`);
+    console.log(`Pixabay Key: ${pixabayKey}`);
 
     // TODO convert country name to ISO-3166 country code 
 
     // Call Geonames API to get coords
-    const coords = await getGeonamesCoords(geoNamesKey, destCity, destCountry);
-    const lat = coords['geonames'][0]['lat'];
-    const lng = coords['geonames'][0]['lng'];
-    console.log('Coordinates', lat, lng)
-
+    const coords = await getGeonamesCoords(geoNamesKey, tripData['location']['city'], tripData['location']['country']);
+    tripData['location']['lat'] = coords['geonames'][0]['lat'];
+    tripData['location']['lng'] = coords['geonames'][0]['lng'];
+    console.log('Coordinates', tripData['location']['lat'], tripData['location']['lng']);
 
     // Call Weatherbit API to get forecast
-    const weatherForecast = await getForecastWeather(lat, lng, weatherbitKey);
-    
+    const weatherForecast = await getForecastWeather(tripData['location']['lat'], tripData['location']['lng'], weatherbitKey);
+    tripData['weather'] = weatherForecast;
+
     // API request to get picture of the location
-    const locationImgUrl = await getPixabayImgUrl(pixabayKey, destCountry + ' ' + destCity);
+    const locationImgUrl = await getPixabayImgUrl(pixabayKey, tripData['location']['country'] + ' ' + tripData['location']['city']);
     // TODO: add error handling to display blank image if there are no results 
-    console.log('location image url', locationImgUrl['hits'][0]['webformatURL'])
+    console.log('location image url', locationImgUrl['hits'][0]['webformatURL']);
+    tripData['imgUrl'] = locationImgUrl['hits'][0]['webformatURL'];
 
     // Update the UI
+    console.log(tripData);
 
-
+    // update trips object
+    trips['tripData'].push(tripData);
+    trips['tripCount'] += 1;
+    trips['idCount'] += 1;
+    console.log(trips);
 }
 
 export {
