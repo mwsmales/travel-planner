349e81edac8926935190e9bf46b597a537e5acbb
Michael S <michael.smales@gmail.com>
2022-04-27 20:12:01 -0500

    add function to populate ui with weather forecast from weatherbit

--- src/client/js/uiFunctions.js
+++ src/client/js/uiFunctions.js
@@ -41,6 +41,7 @@ function addTripUi(tripData) {
     // create & append child with the image
     let imgDiv = document.createElement('div');
     imgDiv.innerHTML = `<img src="${imgUrl}" alt="Trip Headline">`;
+    imgDiv.classList += 'tripImg'
     cardDiv.appendChild(imgDiv);
     
     // create & append child div with the tripinfo.  Incl. innerHTML to the tripinfo div with the destination; dates; number of nights
@@ -50,21 +51,40 @@ function addTripUi(tripData) {
         `<h2>${city}, ${country}</h2>
         <h3>${startDate}</h3>`;
         
-    // create and append child div with weatherforecast
-    let weatherDiv = document.createElement('div');
-    weatherDiv.classList += 'weatherForecast';
-    weatherDiv.innerHTML = '<p>Weather Forecast Placeholder</p>';
-    tripInfoDiv.appendChild(weatherDiv);
-    
     // create and append child with tripcontrols.  Incl. innerHTML with a remove trip button
     let controlsDiv = document.createElement('div');
     controlsDiv.classList += 'tripControls';
     controlsDiv.innerHTML = `<input type="button" value="Delete Trip" onclick="Client.deleteTrip(parseInt(${tripId}))">`;
     tripInfoDiv.appendChild(controlsDiv);
-
     cardDiv.appendChild(tripInfoDiv);
+    
+    // create and append child div with weatherforecast
+    let weatherDiv = document.createElement('div');
+    weatherDiv.classList += 'weatherForecast';
+    weatherDiv = createWeatherDiv(tripData, weatherDiv);
+    cardDiv.appendChild(weatherDiv);
+    
     document.getElementById('outputSection').appendChild(fragment);
 }
+
+
+// create the weather div HTML which can be appended to the document when the UI is drawn
+function createWeatherDiv(tripData, weatherDiv) {
+    for (let i = 0; i <= 10; i++) {
+        let dailyWeather = document.createElement('div');
+        let weatherData = tripData['weather']['data'][i];
+        dailyWeather.classList += 'dailyForecast';
+        dailyWeather.id = `trip_${tripData['id']}_forecast_${i+1}`;
+        dailyWeather.innerHTML = 
+        `<img src="./images/${weatherData['weather']['icon']}.png" alt="weather_icon">` +
+        `<p>${weatherData['datetime']}</p>` +
+        `<p>H ${weatherData['high_temp']} C</p>` +
+        `<p>L ${weatherData['low_temp']} C</p>`
+        weatherDiv.appendChild(dailyWeather);
+    }
+    return(weatherDiv)
+}
+
 
 /**
  * Function to populate country drop-down.
