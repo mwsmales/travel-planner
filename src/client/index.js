7e58c1aa0ca0f123e48036af63c8426bba9f50f5
Michael S <michael.smales@gmail.com>
2022-04-29 17:19:56 -0500

    add service workers

--- src/client/index.js
+++ src/client/index.js
@@ -86,6 +86,17 @@ import './images/u00d.png'
 import './images/u00n.png'
 
 
+// register service worker
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/sw.js').then(registration => {
+            console.log('SW registered: ', registration);
+        }).catch(registrationError => {
+            console.log('SW registration failed: ', registrationError);
+        });
+    });
+}
+
 // event listeners
 window.addEventListener('load', (event) => {
     addCountryList();
